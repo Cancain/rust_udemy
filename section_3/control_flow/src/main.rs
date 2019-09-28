5e472af34315989ceae59ad3a7a562af486a90d2
Cancain <eriksson.tomas@gmail.com>
2019-09-28 14:33:39 +0200

    completed section 3

--- section_3/control_flow/src/main.rs
+++ section_3/control_flow/src/main.rs
@@ -81,8 +81,23 @@ fn for_loop() {
     }
 }
 
+fn match_statement() {
+    let country_code = 999;
+
+    let country = match country_code {
+        44 => "UK",
+        46 => "Sweden",
+        7 => "Russia",
+        1...999 => "unknown",
+        _ => "invalid",
+    };
+
+    println!("The country with code {} is {}", country_code, country);
+}
+
 fn main() {
-    for_loop();
+    match_statement();
+    // for_loop();
     // while_and_loop();
     // if_statement();
 }
